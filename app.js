3d67bb7e0ac110dd5b83cb3931fc5ced98900a64
Daymorelah <demola.hussainin@gmail.com>
2018-10-15 09:26:08 +0100

    Setup backend with frameworks and neccesary files
    - Install back-end frameworks need to start thr server
    - Createv entry file that starts the server
    - Create route handler to welcome user to the API
    - Install packages for transpiling
    - Create a start script in package.json to start the server
    - Update ReadMe file

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import routes from './Routes';
+
+const PORT = process.env.PORT || 2033;
+const app = express();
+
+if (app.get('env') !== 'test') {
+  app.use(logger('dev'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+routes(app);
+
+// Catch all invalid routes
+app.all('*', (req, res) => {
+  res.status(404).jsend.error({
+    code: 404,
+    message: 'Page not found',
+  });
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(`An error occurred try to start the sever. Error is ${error}`);
+  } else {
+    console.log(`Server is up and runnig on port ${PORT} ...`);
+  }
+});
+
+export default app;
